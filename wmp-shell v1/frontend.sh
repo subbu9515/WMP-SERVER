6cce9d1cc1243ddc7f0af81babc1b63c8fa8cfb4
subbu9515 <pinisettisubbu@gmail.com>
2026-05-17 17:46:53 +0530

    sample shell file

---
+++ wmp-shell v1/frontend.sh
@@ -0,0 +1,28 @@
+dnf module disable nginx -y
+dnf module enable nginx:1.26 -y
+dnf install -y nginx
+
+systemctl enable nginx
+systemctl start nginx
+
+curl -fsSL https://rpm.nodesource.com/setup_22.x | bash -
+dnf install -y nodejs
+
+node --version
+npm --version
+
+curl -L -o /tmp/frontend.tar.gz https://raw.githubusercontent.com/raghudevopsb88/wealth-project/main/artifacts/frontend.tar.gz
+mkdir -p /tmp/frontend
+cd /tmp/frontend
+tar xzf /tmp/frontend.tar.gz
+
+cd /tmp/frontend
+npm ci
+npm run build
+
+rm -rf /usr/share/nginx/html/*
+cp -r /tmp/frontend/dist/* /usr/share/nginx/html/
+
+cp nginx.conf /etc/nginx/nginx.conf
+
+systemctl restart nginx
